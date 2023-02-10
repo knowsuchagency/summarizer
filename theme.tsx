730baacf903b82f0b6e913f44fa7fcbb9b5fdd47
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2023-02-10 08:21:05 -0800

    use system color scheme

--- theme.tsx
+++ theme.tsx
@@ -1,10 +1,16 @@
-import { MantineProvider } from "@mantine/core"
 import type { EmotionCache } from "@mantine/core"
+import { MantineProvider } from "@mantine/core"
 import type { PropsWithChildren } from "react"
-import {createStyles} from "@mantine/core";
 
 interface Props extends PropsWithChildren {
   emotionCache?: EmotionCache
+}
+
+function systemDark() {
+  return (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  )
 }
 
 export function ThemeProvider({ emotionCache, children }: Props) {
@@ -14,7 +20,7 @@ export function ThemeProvider({ emotionCache, children }: Props) {
       withNormalizeCSS
       emotionCache={emotionCache}
       theme={{
-        colorScheme: "dark",
+        colorScheme: systemDark() ? "dark" : "light"
       }}>
       {children}
     </MantineProvider>
